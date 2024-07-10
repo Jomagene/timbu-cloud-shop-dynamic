bd67b1095a9e8979688483b8b751a4166ed22c7d
Jomagene <semjomagene@gmail.com>
2024-07-10 15:30:16 +0200

    Fix the scrolrestauration

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
+import { ScrollRestoration } from "react-router-dom";
 
 function Layout() {
   return (
     <>
       <Header />
+      <ScrollRestoration />
       <Outlet />
       <Footer />
     </>
