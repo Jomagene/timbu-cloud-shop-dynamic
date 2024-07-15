1926805f1a156d073837fdc4074102878c49a36c
Jomagene <semjomagene@gmail.com>
2024-07-15 15:32:26 +0200

    Make all images links functional

--- src/components/global/Product.jsx
+++ src/components/global/Product.jsx
@@ -5,17 +5,11 @@ function Product({ picture, title, price, like }) {
   return (
     <div className="flex flex-col items-center">
       <div className="relative h-[150px] w-[100px] sm:h-[210px] sm:w-[140px] md:h-[240px] md:w-[160px] lg:h-[360px] lg:w-[240px]">
-        {picture == pant2 ? (
-          <Link to="/product">
-            <div
-              className="border border-solid border-[#FF5F00] rounded-[10px] rounded-tr-[5px] h-full w-full bg-cover"
-              style={{ backgroundImage: `url(${picture})` }}></div>
-          </Link>
-        ) : (
+        <Link to="/product">
           <div
             className="rounded-[10px] rounded-tr-[5px] h-full w-full bg-cover"
             style={{ backgroundImage: `url(${picture})` }}></div>
-        )}
+        </Link>
 
         {like && (
           <div className="cursor-pointer hover:bg-[#d45e2f] active:bg-[#d45e2fe0] absolute top-0 right-0 bg-[#ced0ce] rounded-[5px] h-[25px] sm:h-[30px] lg:h-[40px] w-[25px] sm:w-[30px] lg:w-[40px] flex items-center justify-center">
